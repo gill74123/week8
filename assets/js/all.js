107dbb05c15ce75cd8225fc4c00a029b77e2d3d0
Gill Chin <gill74123@gmail.com>
2021-08-25 18:14:00 +0800

    Update 2021-08-25T10:13:55.351Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,33 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+  // courseSwiper
+  var swiper = new Swiper(".courseSwiper", {
+    // 循環
+    loop: true,
+    // 自動播放
+    // autoplay: true,
+    speed: 700,
+    // 斷點可看見的slide數量
+    breakpoints: {
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      992: {
+        slidesPerView: 1.3,
+        spaceBetween: 30
+      }
+    }
+  }); // commentSwiper
+
+  var swiper = new Swiper(".commentSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
